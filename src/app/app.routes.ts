e93c43ca9c4d862aa3e10ef6ff93324e3f09098b
babacar <nguiranebabacar305@gmail.com>
2024-12-10 23:37:58 +0000

    code init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'home',
+        component : HomeComponent,
+        title : 'Acceuil'
+    },
+    {path : '', redirectTo : '/home', pathMatch : 'full'}
+
+];
