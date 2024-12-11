ddae52919e435f34ef99e0d8de8bc692f35b42d5
babacar <nguiranebabacar305@gmail.com>
2024-12-11 23:18:00 +0000

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { gsap } from "gsap";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
 
+  ngAfterViewInit(): void {
+    gsap.from('#imageHead',{
+      opacity : 0,
+      duration : 3,
+      translateX : 200,
+    })
+
+    gsap.from('#accroche',{
+      duration : 1,
+      translateY : 100,
+    })
+    
+    gsap.from('#desc',{
+      duration : 1,
+      translateY : 100,
+    })
+
+  }
 }
