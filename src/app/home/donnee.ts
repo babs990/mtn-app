06b6e5eb438c8401ca291bc3d794ad0b582c0137
babacar <nguiranebabacar305@gmail.com>
2024-12-14 16:24:14 +0000

    home

--- src/app/home/donnee.ts
+++ src/app/home/donnee.ts
@@ -15,3 +15,32 @@ export const stat = [
         icon : 'client.svg'
     }
 ]
+
+export const services =[
+    {
+        icon : 'consultance.svg',
+        nom : 'Consultance en banque et finance',
+        txt : 'Nous apportons notre expertise pour aider les institutions financières et bancaires à optimiser leurs opérations, améliorer leur efficacité et renforcer leur compétitivité.'
+    },
+    {
+        icon : 'conseil.svg',
+        nom : 'Conseil et accompagnement',
+        txt : 'Nous travaillons étroitement avec nos clients pour leur offrir un accompagnement complet, de la phase de diagnostic à la mise en œuvre de solutions personnalisées.'
+    },
+    {
+        icon : 'analyse.svg',
+        nom : 'Analyse financière et accompagnement',
+        txt : 'Grâce à des outils d\'analyse performants, nous fournissons des évaluations financières détaillées pour aider nos clients à prendr des décisions informées et stratégiques.'
+    },
+    {
+        icon : 'investissement.svg',
+        nom : 'Investissement',
+        txt : 'MTN GROUP IS conseille ses clients dans leurs stratégies d\'investissement, en identifiant les opportunités les plus prometteuses et en évaluant les risques associés.'
+    },
+    {
+        icon : 'projet.svg',
+        nom : 'Gestion de projet',
+        txt : 'Notre cabinet gère des projets complexes avec une méthodologie rigoureuse, garantissant ainsi la réussite de chaque étape du processus.'
+    }
+
+]
